61f242babfa33574207db2a069ad86c63268aa6d
Agustín <jagustin.rm@gmail.com>
2025-01-11 10:42:49 -0300

    imagen de armaduras para netlify

--- src/loaders/ArmorsLoader.ts
+++ src/loaders/ArmorsLoader.ts
@@ -5,21 +5,34 @@ import { Armor } from '../stores/types/armor';
 const ArmorsLoader = () => {
   const { areArmorsLoaded, setArmors } = useArmorStore();
 
-  // Importa todas las imágenes como objetos con propiedades `.default`
-  const images = import.meta.glob('/src/assets/img/armors/*.png', { eager: true });
+  // Importa todas las imágenes como promesas de módulos con .default
+  const images = import.meta.glob('/src/assets/img/armors/*.png') as Record<
+    string,
+    () => Promise<{ default: string }>
+  >;
 
   useEffect(() => {
     if (!areArmorsLoaded) {
       const loadArmors = async () => {
         try {
           const res = await fetch('/mocks/armors.json');
-          const data = await res.json();
+          const data: Armor[] = await res.json();
 
-          // Reemplaza la propiedad img con las rutas procesadas usando `.default`
-          const armorsWithImages = data.map((armor: Armor) => ({
-            ...armor,
-            img: images[`/src/assets/img/armors/${armor.img}`], // Ya puedes usar .default implícitamente
-          }));
+          // Mapea las armaduras y resuelve las rutas de las imágenes
+          const armorsWithImages = await Promise.all(
+            data.map(async (armor) => {
+              const imagePath = `/src/assets/img/armors/${armor.img}`;
+              if (images[imagePath]) {
+                try {
+                  const module = await images[imagePath]();
+                  return { ...armor, img: module.default };
+                } catch {
+                  console.error(`Error loading image for ${armor.img}`);
+                }
+              }
+              return armor; // Si no se encuentra la imagen, devuelve el objeto original
+            })
+          );
 
           setArmors(armorsWithImages);
         } catch (error) {
